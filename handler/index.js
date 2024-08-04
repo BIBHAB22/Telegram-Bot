a9072c0bc332f7c059be17348b71cb2572a2aa31
BIBHAB22 <bibhabindia@gmail.com>
2024-08-04 19:56:01 +0530

    webseries section done

--- handler/index.js
+++ handler/index.js
@@ -6,9 +6,9 @@ const token = '6654683866:AAG6uiqmiszwv76mMBSMkvUsqPWZ78Auz5U';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Welcome to the Media Bot! Type /movie or /webseries to explore.');
-  });
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Welcome to the Media Bot! Type /movie or /webseries to explore.');
+});
 
 bot.onText(/\/movie (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
@@ -25,11 +25,35 @@ bot.onText(/\/webseries (.+)/, async (msg, match) => {
 const sendMedia = async (chatId, mediaType, mediaName) => {
   try {
     await connectDB();
-    const media = await Media.findOne({ type: mediaType, name: mediaName });
-    if (media) {
-      await bot.sendMessage(chatId, `Here is your ${mediaType}: ${mediaName}\n${media.url}`);
+    let media;
+    if (mediaType === 'web series') {
+      // Find multiple entries for web series
+      // Ensure the keyword is a string and use it in a case-insensitive regex
+      const regex = new RegExp(mediaName, 'i');
+
+      // Find all media documents where type is 'web series' and name matches the regex
+      const webSeries = await Media.find({
+        type: 'web series',
+        name: { $regex: regex }
+      });
+      // Extract URLs from the result
+      const urls = webSeries.map(series => series.url);
+      const names = webSeries.map(series => series.name);
+      if (urls) {
+        for(var i = 0; i < urls.length; i++) {
+          await bot.sendMessage(chatId, `Here is your ${mediaType}: ${names[i]}\n${urls[i]}`);
+        }
+      } else {
+        await bot.sendMessage(chatId, `${mediaType} not found.`);
+      }
     } else {
-      await bot.sendMessage(chatId, `${mediaType} not found.`);
+      // Find a single entry for movies
+      media = await Media.findOne({ type: mediaType, name: mediaName });
+      if (media) {
+        await bot.sendMessage(chatId, `Here is your ${mediaType}: ${mediaName}\n${media.url}`);
+      } else {
+        await bot.sendMessage(chatId, `${mediaType} not found.`);
+      }
     }
   } catch (error) {
     console.error('Error sending media:', error);
@@ -38,4 +62,3 @@ const sendMedia = async (chatId, mediaType, mediaName) => {
     await closeDBConnection();
   }
 };
-
